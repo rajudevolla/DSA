0d1cb7cb8946cbb7f6467425438abc7c3d445835
rajudevolla <47508463+rajudevolla@users.noreply.github.com>
2021-05-15 20:43:12 +0530

    Update rotateRingsOfMatrixByKElements.py
    resolved syntax errors

--- rotateRingsOfMatrixByKElements.py
+++ rotateRingsOfMatrixByKElements.py
@@ -16,23 +16,24 @@ while(rowStartIndex<rowEndIndex and columnStartIndex<columnEndIndex):
     ring=[]
     for i in range(columnStartIndex,columnEndIndex):
         ring.append(mat[rowStartIndex][i])
-        rowStartIndex+=1
+    rowStartIndex+=1
     for i in range(rowStartIndex,rowEndIndex):
         ring.append(mat[i][columnEndIndex-1])
-        columnEndIndex=columnEndIndex-1
+    columnEndIndex=columnEndIndex-1
     if(rowStartIndex<rowEndIndex):
         for i in range(columnEndIndex-1,columnStartIndex-1,-1):
             ring.append(mat[rowEndIndex-1][i])
-            rowEndIndex=rowEndIndex-1
+        rowEndIndex=rowEndIndex-1
     if(columnStartIndex<columnEndIndex):
         for i in range(rowEndIndex-1,rowStartIndex-1,-1):
             # print(matrix[i][columnStartIndex])
             ring.append(mat[i][columnStartIndex])
-            columnStartIndex+=1
+        columnStartIndex+=1
     layers.append(ring)
 print(layers)
+ans=""
 for layer in layers:
     if(len(layer)>rot):
         layer[:]=layer[rot:]+layer[:rot]
-        ans=ans+layer.join('')
+    ans=ans+str(layer)
 print(ans)
